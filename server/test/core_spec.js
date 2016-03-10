8b3d454724e86edc182737fe0de0b330d3d8c49e
Guilherme Berger <guilherme.berger@gmail.com>
2016-03-10 12:11:01 -0300

    next

--- server/test/core_spec.js
+++ server/test/core_spec.js
@@ -1,12 +1,11 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
 
   describe('setEntries', () => {
-
     it('adds the entries to the state', () => {
       const state = Map();
       const entries = ['Trainspotting', '28 Days Later'];
@@ -15,6 +14,20 @@ describe('application logic', () => {
         entries: List.of('Trainspotting', '28 Days Later')
       }));
     });
+  });
 
-  })
+  describe('next', () => {
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        entries: List.of('Sunshine'),
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        })
+      }));
+    });
+  });
 });
